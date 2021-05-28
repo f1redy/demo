da72e58f2550df3da971d1c7432c85516000191f
GTOrdonezFre <fredy.ordonez@grupopaf.com.gt>
2021-05-28 08:12:20 -0600

    first commit

---
+++ Core/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion.site.Core
+{
+    public static class RolAcceso
+    {
+        public const string ADMIN = "ADMIN";
+        public const string USER = "USER";
+        public const string DIRECTOR = "DIRECTOR";
+        public const string SUPER = "SUPER";
+    }
+
+    public class Utils
+    {
+
+        public static string CifrarTexto(string texto)
+        {
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] bytes = encoding.GetBytes(texto);
+            SHA512Managed sha512 = new SHA512Managed();
+            return encoding.GetString(sha512.ComputeHash(bytes));
+        }
+
+        public static bool ValidarTexto(string texto, string textoCifrado)
+        {
+            string valorCifrado = CifrarTexto(texto);
+            UnicodeEncoding encoding = new UnicodeEncoding();
+            byte[] bytesIngreso = encoding.GetBytes(valorCifrado);
+            byte[] bytesReferencia = encoding.GetBytes(textoCifrado);
+            return ValidarBytes(bytesIngreso, bytesReferencia);
+        }
+
+        private static bool ValidarBytes(byte[] bytes1, byte[] bytes2)
+        {
+            if (bytes1.Length != bytes2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < bytes1.Length; i++)
+            {
+                if (!(bytes1[i].Equals(bytes2[i])))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
